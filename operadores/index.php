6dbb56e8863373832e2f1c042198295269394a25
Luiz Bono <lfamlfam@gmail.com>
2020-08-20 22:11:06 -0300

    Exemplo de operadores rodando já com servidor web

---
+++ operadores/index.php
@@ -0,0 +1,30 @@
+<?php
+
+//Comentário de linha
+
+/*
+ *
+ *  Comentário de bloco
+ *
+ */
+
+$saldo = 34.5;//Operador de atribuição
+
+//Aspas duplas aceitam string especial
+echo "<br><br><br>Seu saldo é $saldo ";
+
+//Aspas simples apenas string pura
+echo '<br><br><br>Seu saldo é $saldo ';
+
+$saldo = $saldo - 10;//Operador de subtração
+
+echo "<br><br><br>Agora seu saldo é $saldo ";
+
+$saldo -= 10;//Operador de subtração
+
+echo "<br><br><br>Agora seu saldo é $saldo ";
+
+$saldo *= 1000;//Operador de multiplicação
+
+echo "<br><br><br>Agora seu saldo é $saldo ";
+
