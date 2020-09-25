daab2254bd53bdbe5e50461ed81b322c65aa71d5
Luiz Bono <lfamlfam@gmail.com>
2020-09-24 22:22:08 -0300

    Aula finalizada

--- micro_app/edita_contato_tpl.php
+++ micro_app/edita_contato_tpl.php
@@ -6,6 +6,10 @@
 			<a href='lista_contato.php'>Editar</a> 
 			<a href='lista_contato.php'>Apagar</a><br><br>
 			<h1>Edite seu contato</h1>
+			<br>
+			<?php 
+			if ( isset($msg) ) echo $msg; 
+			?>
 			<br><br>
 			<form method="post" action="edita_contato.php">
 				Nome: <input type="text" name="nm" value="<?php echo $contato['nome'] ?>"> 
